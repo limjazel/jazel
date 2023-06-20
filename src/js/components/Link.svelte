ea38e790eb605244d34051af3ed0704fab14ba9a
Jazel Lim <lim.jazel@gmail.com>
2023-06-20 21:47:01 +0800

    Add Link component

---
+++ src/js/components/Link.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let href;
+  export let external = false;
+  export let type = "stroke";
+  export { className as class };
+
+  const baseStyle = `[ cursor-pointer underline underline-offset-2 decoration-2 ] [ font-semibold ]`;
+
+  const types = {
+    accent: "c-link--accent [ text-accent ]",
+    stroke: "c-link--stroke [ text-stroke ]",
+  };
+
+  let className = "";
+</script>
+
+<a
+  {href}
+  on:click
+  target={external ? "_blank" : "_self"}
+  rel={external && "nofollow noreferrer"}
+  class="c-link {baseStyle} {types[type]} {className}"
+>
+  <slot />
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import Button from "../js/components/Button.svelte";
+  import Link from "../js/components/Link.svelte";
 </script>
 
 <p class="text-3xl">
@@ -27,11 +28,18 @@
       <Button type="neutral">Neutral</Button>
       <Button type="neutral" disabled>Neutral</Button>
     </div>
-    
+
     <div class="[ flex flex-col items-start gap-4 ]">
       <Button type="neutral" size="small">Neutral</Button>
       <Button type="neutral" size="base">Neutral</Button>
       <Button type="neutral" size="large">Neutral</Button>
     </div>
   </div>
+</div>
+
+<div class="[ mt-10 py-10 ]">
+  <h2>Link</h2>
+
+  <Link href="/design">I will go to design page</Link>
+  <Link href="/design" external={true} type="accent" class="[ bg-stroke ]">I will open a new tab to design page</Link>
 </div>
