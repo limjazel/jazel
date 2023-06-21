f5c2ee32ba14ccdaa59364a49c4891de2dc3fb0c
Jazel Lim <lim.jazel@gmail.com>
2023-06-21 20:05:35 +0800

    Card CS fix

--- src/js/components/Card.svelte
+++ src/js/components/Card.svelte
@@ -3,6 +3,6 @@
   let className = "";
 </script>
 
-<div class="c-card [ p-8 rounded-xl shadow ] [ bg-canvas hover:bg-pearl ]">
+<div class="c-card [ p-8 w-full rounded-xl shadow ] [ bg-canvas ] {className}">
   <slot />
 </div>
