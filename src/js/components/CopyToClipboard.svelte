ee4b5c1d5332775533d68f6204eeb47bbe1c38b4
Jazel Lim <lim.jazel@gmail.com>
2023-06-21 23:36:30 +0800

    Fix notification not disappearing after button click

--- src/js/components/CopyToClipboard.svelte
+++ src/js/components/CopyToClipboard.svelte
@@ -1,11 +1,11 @@
 <script>
   import { onMount } from "svelte";
-  import { scale } from "svelte/transition"
+  import { scale } from "svelte/transition";
 
   export let id = "";
   export let name = "";
   export let value = "";
-  export let timeout = 3000;
+  export let timeout = 4000;
 
   let timer;
   let notification = false;
@@ -28,6 +28,7 @@
             "Could not copy the URL to the clipboard, please try again.";
         });
     }
+    timer = setTimeout(() => close(), timeout);
   }
 
   function close() {
@@ -47,26 +48,6 @@
   });
 </script>
 
-{#if notification}
-  <div
-    class="[ fixed bottom-0 right-0 px-4 pb-4 max-w-[340px] md:max-w-[360px] ]"
-  >
-    <div
-      on:mouseenter={handleMouseEnter}
-      on:mouseleave={handleMouseLeave}
-      transition:scale={{ duration: 300 }}
-      class="[ flex flex-col ] [ bg-canvas rounded shadow px-8 py-5 ]"
-    >
-      <button
-        on:click={() => (notification = false)}
-        class="[ absolute -mt-3 right-6 p-2 bg-red-100 ]">x</button
-      >
-      <span class="[ font-bold ]">{notificationTitle}</span>
-      <span>{notificationMessage}</span>
-    </div>
-  </div>
-{/if}
-
 <label for={id} class="[ sr-only ]"> Copy URL </label>
 
 <input
@@ -80,3 +61,23 @@
 />
 
 <slot {copy} />
+
+{#if notification}
+  <div
+    class="[ fixed bottom-0 right-0 px-4 pb-4 max-w-[340px] md:max-w-[360px] ] [ z-20 ]"
+  >
+    <div
+      on:mouseenter={handleMouseEnter}
+      on:mouseleave={handleMouseLeave}
+      transition:scale={{ duration: 300 }}
+      class="[ flex flex-col ] [ bg-white rounded shadow px-8 py-5 ]"
+    >
+      <button
+        on:click={() => (notification = false)}
+        class="[ absolute -mt-3 right-6 p-2 bg-red-100 ]">x</button
+      >
+      <span class="[ font-bold ]">{notificationTitle}</span>
+      <span>{notificationMessage}</span>
+    </div>
+  </div>
+{/if}
