0575d0593c9357917665171a51b0a4b959f8238e
Jazel Lim <lim.jazel@gmail.com>
2023-06-24 01:49:16 +0800

    Auto update dropdown position on screen resize using floating-ui

--- src/js/components/Dropdown.svelte
+++ src/js/components/Dropdown.svelte
@@ -1,16 +1,33 @@
 <script>
   import { clickOutside } from "../actions/on-click-outside";
+  import { onDestroy } from "svelte";
+  import {
+    computePosition,
+    flip,
+    offset,
+    autoUpdate,
+    shift,
+  } from "@floating-ui/dom";
+
+  export let placement = "bottom-start";
+  export let middleware = [flip(), offset(4), shift({ padding: 4 })];
 
   let open = false;
   let selected = "";
+  let trigger;
   let menu;
+  let cleanup;
 
   function toggleMenu() {
+    updateMenuPosition();
     open = !open;
   }
 
   function closeMenu() {
     open = false;
+    if (typeof cleanup === "function") {
+      cleanup();
+    }
   }
 
   function handleKeydown(event) {
@@ -18,16 +35,34 @@
       closeMenu();
     }
   }
+
+  function updateMenuPosition() {
+    cleanup = autoUpdate(trigger, menu, () => {
+      computePosition(trigger, menu, {
+        placement: placement,
+        middleware: middleware,
+      }).then(({ x, y }) => {
+        Object.assign(menu.style, {
+          left: `${x}px`,
+          top: `${y}px`,
+        });
+      });
+    });
+  }
+
+  onDestroy(() => {
+    if (typeof cleanup === "function") {
+      cleanup();
+    }
+  });
 </script>
 
 <svelte:window on:keydown={handleKeydown} />
 
-<div
-  use:clickOutside
-  on:click-outside={() => (open = false)}
-  class="[ inline-block ]"
->
-  <slot name="trigger" {toggleMenu} {closeMenu} />
+<div use:clickOutside on:click-outside={closeMenu} class="[ inline-block ]">
+  <div bind:this={trigger}>
+    <slot name="trigger" {toggleMenu} {closeMenu} />
+  </div>
 
   <nav
     bind:this={menu}
