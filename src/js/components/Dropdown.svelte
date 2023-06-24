a8234d9d6624ccde6b16778e5e8b27900e8d7b8f
Jazel Lim <lim.jazel@gmail.com>
2023-06-25 02:56:11 +0800

    Update Dropdown floating-ui dependency

--- src/js/components/Dropdown.svelte
+++ src/js/components/Dropdown.svelte
@@ -1,35 +1,30 @@
 <script>
   import { clickOutside } from "../actions/on-click-outside";
-  import { onDestroy } from "svelte";
-  import {
-    computePosition,
-    flip,
-    offset,
-    autoUpdate,
-    shift,
-  } from "@floating-ui/dom";
+  import { offset, shift, flip } from "svelte-floating-ui/dom";
+  import { createFloatingActions } from "svelte-floating-ui";
 
   export let placement = "bottom-end";
   export let middleware = [flip(), offset(4), shift({ padding: 4 })];
   export { className as class };
 
+  const [floatingRef, floatingContent] = createFloatingActions({
+    strategy: "absolute",
+    placement: placement,
+    middleware: middleware,
+  });
+
   let open = false;
   let selected = "";
   let trigger;
   let menu;
-  let cleanup;
   let className = "";
 
   function toggleMenu() {
-    updateMenuPosition();
     open = !open;
   }
 
   function closeMenu() {
     open = false;
-    if (typeof cleanup === "function") {
-      cleanup();
-    }
   }
 
   function handleKeydown(event) {
@@ -37,37 +32,18 @@
       closeMenu();
     }
   }
-
-  function updateMenuPosition() {
-    cleanup = autoUpdate(trigger, menu, () => {
-      computePosition(trigger, menu, {
-        placement: placement,
-        middleware: middleware,
-      }).then(({ x, y }) => {
-        Object.assign(menu.style, {
-          left: `${x}px`,
-          top: `${y}px`,
-        });
-      });
-    });
-  }
-
-  onDestroy(() => {
-    if (typeof cleanup === "function") {
-      cleanup();
-    }
-  });
 </script>
 
 <svelte:window on:keydown={handleKeydown} />
 
 <div use:clickOutside on:click-outside={closeMenu} class="c-dropdown">
-  <div bind:this={trigger}>
+  <div bind:this={trigger} use:floatingRef>
     <slot name="trigger" {toggleMenu} {closeMenu} />
   </div>
 
   <div
     bind:this={menu}
+    use:floatingContent
     class="[ absolute z-10 ] [ grid grid-col max-w-[16rem] ] [ rounded mt-2 ] {className}"
     class:hidden={!open}
   >
