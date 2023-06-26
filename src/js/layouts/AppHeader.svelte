6fe3d161700b7a008d4373fc9d43514d03d413c8
Jazel Lim <lim.jazel@gmail.com>
2023-06-26 17:19:39 +0800

    Update logo and favicon

--- src/js/layouts/AppHeader.svelte
+++ src/js/layouts/AppHeader.svelte
@@ -5,15 +5,23 @@
 <header>
   <nav
     aria-label="primary navigation"
-    class="[ py-4 md:py-6 flex flex-wrap items-center gap-x-10 ]"
+    class="[ py-4 flex flex-wrap md:flex-nowrap items-center justify-between gap-2 md:gap-10 ]"
   >
     <Link href="/">
       <span class="sr-only">go to home</span>
-      <span class="font-heading text-3xl text-stroke tracking-tight italic">Jazel Lim</span>
+      <span class="[ flex md:min-w-[12rem] relative -top-0.5 ]">
+        <img
+          src="/images/logos/main-logo-w-color.svg"
+          alt="a hand drawn logo"
+          class="[ w-full object-contain ]"
+        />
+      </span>
+      <span class="[ sr-only ]"> Jazel Lim </span>
     </Link>
 
-    <Link href="/">home</Link>
-
-    <Link href="/design">design</Link>
+    <ul class="[ flex gap-6 ]">
+      <Link href="/">home</Link>
+      <Link href="/design">design</Link>
+    </ul>
   </nav>
 </header>
